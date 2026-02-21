f92fb475e68827cf7e0f13d8c9ec8039a7d1f29d
EDVAN LEITE DA SILVA SANTOS <31933205873@info.local>
2026-02-21 14:49:36 -0300

    continuação

--- ExercicioFILTER.js
+++ ExercicioFILTER.js
@@ -1,5 +1,14 @@
+//FILTER - Exercicio 1
 let notas = [4, 7, 9, 3, 10, 5];
 
 let maiorIgualSete = notas.filter(nota => nota >= 7);
 console.log("maiores e igual sete: ", maiorIgualSete);
 
+//FILTER - Exercicio 2
+const palavras = ["sol", "mar", "computador", "lua", "código"];
+
+const maisQueQuatroLetras = palavras.filter(function(letras){
+    return letras.length > 4;
+});
+
+console.log("Palavras com mais de 4 letras:  ", maisQueQuatroLetras);
